4322b5bc54b4b2bad9dbf528bc7c4e6f99b8da20
thiefm <volzh34rus@yandex.ru>
2020-02-11 23:54:48 +0400

    in self_task\work_11.02.2020\projects\ConsoleApp1

---
+++ self_task/work_11.02.2020/projects/ConsoleApp1/ConsoleApp1/ArraySort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    static class ArraySort
+    {
+        public static void BubbleSort(ref int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return;
+
+            bool isSwap = false;
+            while (true)
+            {
+                isSwap = false;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        int tmp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = tmp;
+                        isSwap = true;
+                    }
+                }
+                if (!isSwap)
+                    break;
+            }
+        }
+        public static void SelectionSort(ref int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                    if (array[maxIndex] < array[j])
+                        maxIndex = j;
+
+                if (maxIndex != i)
+                {
+                    int tmp = array[i];
+                    array[i] = array[maxIndex];
+                    array[maxIndex] = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ self_task/work_11.02.2020/projects/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array01 = { 5, 1, 3, 10, 20, 4, 16 };
+            int[] array02 = { 5, 1, 3, 10, 20, 4, 16 };
+
+            ArraySort.BubbleSort(ref array01);
+            foreach (int elem in array01)
+                Console.Write(elem + " ");
+
+            Console.WriteLine();
+
+            ArraySort.SelectionSort(ref array02);
+            foreach (int elem in array02)
+                Console.Write(elem + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
